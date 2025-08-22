d031eb3c89d63f312c23978010da2a9965095dc3
Mariaferb <maria_vargasmo@fet.edu.co>
2025-08-22 17:42:19 -0500

    Trabajo 2 eliminar elementos de la lista

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-                        const tareaInput = document.getElementById('demoTareaInput');
-                        const agregarBtn = document.getElementById('demoAgregarBtn');
-                        const listaTareas = document.getElementById('demoListaTareas');
-                        
+var lista = [];
+const tareaInput = document.getElementById('demoTareaInput');
+const agregarBtn = document.getElementById('demoAgregarBtn');
+const listaTareas = document.getElementById('demoListaTareas');
 
-                        agregarBtn.addEventListener('click', function() {
-                        const nuevaTareaTexto = tareaInput.value;
-                        if (nuevaTareaTexto === '') return;
-                        
+agregarBtn.addEventListener('click', function () {
+    const nuevaTareaTexto = tareaInput.value.trim();
+    if (nuevaTareaTexto === '') return;
 
-                        const nuevaTareaLi = document.createElement('li');
-                        nuevaTareaLi.textContent = nuevaTareaTexto;
-                        listaTareas.appendChild(nuevaTareaLi);
-                        
+    const nuevaTareaLi = document.createElement('li');
 
-                        tareaInput.value = '';
-                        });
+    const textoTarea = document.createElement('span');
+    textoTarea.textContent = nuevaTareaTexto;
 
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'X';
+    botonEliminar.style.marginLeft = '10px';
 
+    botonEliminar.addEventListener('click', function () {
+        listaTareas.removeChild(nuevaTareaLi);
+    });
+    nuevaTareaLi.appendChild(textoTarea);
+    nuevaTareaLi.appendChild(botonEliminar);
 
+    listaTareas.appendChild(nuevaTareaLi);
 
-
-
+    tareaInput.value = '';
+});
